02c11848871c5d0bee6c29b515b75e779b5fb7ad
Bartek_Anczewski <s094217@student.tu.kielce.pl>
2023-10-21 22:41:25 +0200

    added different things

--- src/main/java/pl/redred/WorkerLibrary/LibraryController.java
+++ src/main/java/pl/redred/WorkerLibrary/LibraryController.java
@@ -6,26 +6,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/library")
+@RequestMapping("/library") // Adnotacja określa ścieżkę bazową dla wszystkich metod w tym kontrolerze.
 public class LibraryController {
 
-    @Autowired
+    @Autowired // Wstrzykiwanie zależności dla WorkerRepository.
     WorkerRepository workerRepository;
 
+    // Metoda zwracająca listę wszystkich pracowników.
     @GetMapping("")
     public List<Worker> getAll(){
         return workerRepository.getAll();
     }
 
+    // Metoda zwracająca pracownika na podstawie podanego id.
     @GetMapping("/{id}")
     public Worker getById(@PathVariable("id")int id){
         return workerRepository.getById(id);
     }
 
+    // Metoda dodająca listę pracowników do bazy danych.
     @PostMapping("")
     public int add(@RequestBody List<Worker>workers){
         return workerRepository.save(workers);
     }
+
+    // Metoda aktualizująca dane pracownika w bazie danych.
     @PutMapping("/{id}")
     public int update(@PathVariable("id")int id, @RequestBody Worker updateWorker){
         Worker worker = workerRepository.getById(id);
@@ -40,6 +45,7 @@ public class LibraryController {
         }
         return 1;
     }
+    // Metoda częściowo aktualizująca dane pracownika w bazie danych.
     @PatchMapping("/{id}")
     public int partiallyUpdate(@PathVariable("id")int id, @RequestBody Worker updatedWorker){
         Worker worker = workerRepository.getById(id);
@@ -55,8 +61,22 @@ public class LibraryController {
             return -1;
         }
     }
+
+    // Metoda usuwająca pracownika na podstawie podanego id.
     @DeleteMapping("/{id}")
     public int delete (@PathVariable("id")int id ){
         return workerRepository.delete(id);
+    }
+
+    //Metoda zwracająca pracowników którzy zarabiają wyższą pensję niż podana pensja
+    @PostMapping("/salary/{salary}")
+    public List<Worker> searchSalary(@PathVariable("salary") double salary){
+        return workerRepository.searchSalary(salary);
+    }
+
+    //Metoda zwracająca pracowników o podanym stanowisku
+    @PostMapping("/position/{position}")
+    public List<Worker> searchPosition(@PathVariable("position") String position){
+        return workerRepository.searchPosition(position);
     }
 }
